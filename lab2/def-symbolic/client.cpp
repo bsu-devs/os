8d14ffc15af8549a7729ae292a7819773a45cb68
iborsch <iborsch@users.noreply.github.com>
2016-03-09 21:41:41 +0300

    Update client.cpp

--- lab2/def-symbolic/client.cpp
+++ lab2/def-symbolic/client.cpp
@@ -4,29 +4,39 @@
 
 using namespace std;
 
-int main () {
-	HMODULE hLib = LoadLibraryA("lib.dll");
+class IMyObject {
+public:
+	virtual char* getName() = 0;
+	virtual void setName(const char*&) = 0;
+};
+int main() {
+	HMODULE hLib = LoadLibraryA("../Debug/def_numeric.dll");
 	if (!hLib) throw GetLastError();
 
 	// Test for exported variable timestamp
-	time_t timestamp = (time_t) GetProcAdress(hLib, "timestamp");
+	time_t timestamp = (time_t)GetProcAddress(hLib, "timestamp");
 	cout << "Timestamp: " << timestamp << "\n\n";
-	
+
 	// Test for exported function sum
-	int (*sum)(int, int);
-	sum = (int (*)(int, int)) GetProcAdress(hLib, "sum");
+	int(*sum)(int, int);
+	sum = (int(*)(int, int)) GetProcAddress(hLib, "sum");
 	cout << "2 + 5 = " << sum(2, 5) << "\n\n";
 
 	// Test for exported function whoami
-	void (*whoami)();
-	whoami = (void (*)()) GetProcAdress(hLib, "whoami");
+	char*(*whoami)();
+	whoami = (char*(*)()) GetProcAddress(hLib, "whoami");
 	cout << "WHOAMI: " << whoami() << "\n\n";
 
 	// Test for exported class MyObject
-	MyObject* object = (*MyObject) GetProcAdress(hLib, "object");
+	IMyObject*(*createObject)(const char*&);
+	createObject = (IMyObject*(*)(const char*&)) GetProcAddress(hLib, "createObject");
+	const char *shit = "shit";
+	IMyObject* object = createObject(shit);
 	cout << "Initial name: " << object->getName() << "\n";
-	object->setName("James, Ira, Kun");
+	const char* name = "James, Ira, Kun";
+	object->setName(name);
 	cout << "New name: " << object->getName() << "\n\n";
 
 	FreeLibrary(hLib);
+	system("pause");
 }
