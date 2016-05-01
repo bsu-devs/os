e62d11be2036e7f7b073d78a9db7e4f119e14366
jakwuh <jakwuh@gmail.com>
2016-05-01 23:01:38 +0300

    Remove redundant files

--- lab5/lab5-dll/lab5-dll.Shared/lab5-dll.cpp
+++
@@ -1,2 +0,0 @@
-﻿#include "pch.h"
-#include "lab5-dll.h"
--- lab6/task 2/b.cpp
+++
@@ -1,9 +0,0 @@
-#include <Windows.s>
-#include <conio.h>
-#include <cstdlib>
-#include <stdio.h>
-
-int main() {
- 	while (1) printf("\nProcess B: I will never die!!!\n");
-	return 0;
-}
